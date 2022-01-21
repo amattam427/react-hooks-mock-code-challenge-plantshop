b2b9e588e56eaf6c46b3d14af4fa32bbb93ce888
Ashley Mattam <amattam427@gmail.com>
2022-01-20 21:12:10 -0500

    commit tree 26970f5144b8c89d8314e4652a5f33e2e945c589

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,18 @@ import Header from "./Header";
 import PlantPage from "./PlantPage";
 
 function App() {
+//   const [plantsArray, setPlantsArray] = useState([]);
+//   //console.log(plantsArray)
+// //the const and useEffect could have been done in PlantPage componenet instead. 
+//   useEffect(()=>{
+//     //below = a GET request
+//     fetch('http://localhost:6001/plants')
+//     .then((r)=>r.json())
+//     .then((data)=>{
+//       setPlantsArray(data)
+//     })
+//   }, [])
+
   return (
     <div className="app">
       <Header />
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantsArray, setPlantsArray] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+  //console.log(plantsArray)
+  useEffect(()=>{
+    //below = a GET request
+    fetch('http://localhost:6001/plants')
+    .then((r)=>r.json())
+    .then((data)=>{
+      setPlantsArray(data)
+    })
+  }, [])
+  
+
+
+  const filteredPlants = plantsArray.filter((plant)=>{
+        return plant.name.toLowerCase().includes(searchTerm.toLowerCase());
+          })
+
+  //add handleAddPlant
+  function handleAddPlant(newPlant){
+    const newPlantsArray = [...plantsArray, newPlant];
+    setPlantsArray(newPlantsArray)
+  }
+  //different way to write handleAddPlant::
+  //function handleAddPlant(newPlant){
+    //setPlantsArray((oldPlants)=> [...oldPlants, newPlant])
+  //}
+  
+  function handleDeletePlant(id){
+    const newPlantsArray= plantsArray.filter((plants)=> plants.id !== id);
+    setPlantsArray(newPlantsArray)
+  }
+
+  
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search setSearchTerm={setSearchTerm}/>
+      <PlantList plants={filteredPlants}  onDelete={handleDeletePlant}/>
     </main>
   );
 }
 
 export default PlantPage;
+
+
+
+
+
